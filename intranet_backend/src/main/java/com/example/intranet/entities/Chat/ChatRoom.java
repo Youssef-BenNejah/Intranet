14f259825deaf24f2b8d8a3f8959912159c1438c
bjaouiala <bjaouiala3@gmail.com>
2023-11-25 16:17:59 +0100

    meeting

--- intranet_backend/src/main/java/com/example/intranet/entities/Chat/ChatRoom.java
+++ intranet_backend/src/main/java/com/example/intranet/entities/Chat/ChatRoom.java
@@ -22,6 +22,6 @@ public class ChatRoom {
     private String recipientId;
 
 
-    @OneToMany(mappedBy = "chatRoom",cas)
+    @OneToMany(mappedBy = "chatRoom")
     List<Message> messages = new ArrayList<>();
 }
